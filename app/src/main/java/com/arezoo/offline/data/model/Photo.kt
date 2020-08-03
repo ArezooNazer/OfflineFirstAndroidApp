c9a8d4cf5000eb9e15127df9a8e86ca4b514141a
Nazer <arezoo.nazerdeylami@yaraholding.com>
2020-08-03 06:14:14 +0430

    add Photo model & API

---
+++ app/src/main/java/com/arezoo/offline/data/model/Photo.kt
@@ -0,0 +1,16 @@
+package com.arezoo.offline.data.model
+
+import com.google.gson.annotations.SerializedName
+
+data class Photo(
+    @SerializedName("albumId")
+    var albumId: Int? = null,
+    @SerializedName("id")
+    var id: Int? = null,
+    @SerializedName("title")
+    var title: String? = null,
+    @SerializedName("url")
+    var url: String? = null,
+    @SerializedName("thumbnailUrl")
+    var thumbnailUrl: String? = null
+)
--- app/src/main/java/com/arezoo/offline/data/remote/APIs.kt
+++ app/src/main/java/com/arezoo/offline/data/remote/APIs.kt
@@ -1,4 +1,15 @@
 package com.arezoo.offline.data.remote
 
+import com.arezoo.offline.data.model.Photo
+import io.reactivex.Observable
+import retrofit2.http.GET
+import retrofit2.http.Query
 
-interface APIs {}
+
+interface APIs {
+
+    @GET("photos")
+    fun getPhotos(@Query("_start") start: Int,
+                  @Query("_limit") limit: Int
+    ): Observable<List<Photo>>
+}
