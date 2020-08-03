b674ced786b9c004f349af3916539b051b42bbb8
Nazer <arezoo.nazerdeylami@yaraholding.com>
2020-08-03 06:01:53 +0430

    add MainApplication & MainActivity as a launcher activity

---
+++ app/src/main/java/com/arezoo/offline/presentation/MainApplication.kt
@@ -0,0 +1,13 @@
+package com.arezoo.offline.presentation
+
+import android.app.Application
+import android.content.Context
+import androidx.multidex.MultiDex
+
+class MainApplication: Application() {
+
+    override fun attachBaseContext(base: Context?) {
+        super.attachBaseContext(base)
+        MultiDex.install(this)
+    }
+}
