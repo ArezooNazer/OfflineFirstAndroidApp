95cd4865d4f19d337a48131e760ab65994ed5c27
Nazer <arezoo.nazerdeylami@yaraholding.com>
2020-08-03 07:03:24 +0430

    add fetch Photo list from remote with pagination

--- app/src/main/java/com/arezoo/offline/presentation/MainApplication.kt
+++ app/src/main/java/com/arezoo/offline/presentation/MainApplication.kt
@@ -3,8 +3,26 @@ package com.arezoo.offline.presentation
 import android.app.Application
 import android.content.Context
 import androidx.multidex.MultiDex
+import com.arezoo.offline.di.component.DaggerAppComponent
+import dagger.android.AndroidInjector
+import dagger.android.DispatchingAndroidInjector
+import dagger.android.HasAndroidInjector
+import javax.inject.Inject
 
-class MainApplication: Application() {
+class MainApplication : Application(), HasAndroidInjector {
+
+    @Inject
+    lateinit var androidInjector: DispatchingAndroidInjector<Any>
+    override fun androidInjector(): AndroidInjector<Any> = androidInjector
+
+    override fun onCreate() {
+        super.onCreate()
+
+        DaggerAppComponent
+            .factory()
+            .create(this)
+            .inject(this)
+    }
 
     override fun attachBaseContext(base: Context?) {
         super.attachBaseContext(base)
